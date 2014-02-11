bcae7c6a70af54d5893413a8ca7ecef97aa9d486
sven <sven@so36.net>
2014-02-11 10:22:19 +0100

    Start cleaning

--- slave.py
+++ slave.py
@@ -180,69 +180,6 @@ def setup_python_26():
     sudo('rm -rf /root/tmp')
     test_setup_python_26()
 
-
-def setup_python_26_old():
-    """Install Python 2.6 with Imaging and LXML. This is the old and ugly way
-    with patches and stuff.
-    """
-    # http://ubuntuforums.org/showthread.php?t=1976837
-    if exists('/opt/python-2.6', use_sudo=True):
-        sudo('rm -rf /opt/python-2.6')
-    if exists('/root/tmp', use_sudo=True):
-        sudo('rm -rf /root/tmp')
-    sudo('mkdir /root/tmp')
-    with cd('/root/tmp'):
-        sudo('wget http://python.org/ftp/python/2.6.8/Python-2.6.8.tgz')
-        sudo('tar xfvz Python-2.6.8.tgz')
-    with cd('/root/tmp/Python-2.6.8/'):
-        put('etc/setup.py.patch', '/tmp')
-        sudo('patch setup.py < /tmp/setup.py.patch')
-        put('etc/ssl.patch', '/tmp')
-        sudo('patch Modules/_ssl.c < /tmp/ssl.patch')
-        sudo('env CPPFLAGS="-I/usr/lib/x86_64-linux-gnu" LDFLAGS="-L/usr/include/x86_64-linux-gnu" ./configure --prefix=/opt/python2.6')
-        sudo('make')
-        put('etc/ssl.py.patch', '/tmp')
-        sudo('patch Lib/ssl.py < /tmp/ssl.py.patch')
-        sudo('make install')
-    # PIL
-    sudo('apt-get install -y zlib1g-dev')
-    sudo('apt-get install -y libfreetype6 libfreetype6-dev')
-    sudo('apt-get install -y libjpeg-dev')
-    #http://www.sandersnewmedia.com/why/2012/04/16/installing-pil-virtualenv-ubuntu-1204-precise-pangolin/
-    if not exists('/usr/lib/`uname -i`-linux-gnu/libfreetype.so'):
-        sudo('ln -s /usr/lib/`uname -i`-linux-gnu/libfreetype.so /usr/lib/')
-    if not exists('/usr/lib/`uname -i`-linux-gnu/libjpeg.so'):
-        sudo('ln -s /usr/lib/`uname -i`-linux-gnu/libjpeg.so /usr/lib/')
-    if not exists('/usr/lib/`uname -i`-linux-gnu/libz.so'):
-        sudo('ln -s /usr/lib/`uname -i`-linux-gnu/libz.so /usr/lib/')
-    with cd('/root/tmp'):
-        sudo('wget http://effbot.org/downloads/Imaging-1.1.7.tar.gz')
-        sudo('tar xfvz Imaging-1.1.7.tar.gz')
-    with cd('/root/tmp/Imaging-1.1.7/'):
-        sudo('/opt/python2.6/bin/python setup.py install')
-    # Install BZ2 Support
-    with cd('/root/tmp'):
-        sudo('wget http://labix.org/download/python-bz2/python-bz2-1.1.tar.bz2')
-        sudo('tar xfvj python-bz2-1.1.tar.bz2')
-    with cd('/root/tmp/python-bz2-1.1/'):
-        sudo('/opt/python2.6/bin/python setup.py install')
-    # Install LXML
-    sudo('apt-get install -y libxslt1-dev libxml2-dev')
-    with cd('/root/tmp'):
-        sudo('wget http://pypi.python.org/packages/source/l/lxml/lxml-2.3.6.tar.gz')
-        sudo('tar xfvz lxml-2.3.6.tar.gz')
-    with cd('/root/tmp/lxml-2.3.6/'):
-        sudo('/opt/python2.6/bin/python setup.py install')
-    # Create Symlink
-    if not exists('/usr/local/bin/python2.6'):
-        sudo('ln -s /opt/python2.6/bin/python /usr/local/bin/python2.6')
-    # Clean up
-    sudo('rm -rf /root/tmp')
-    # Install Setuptools
-    sudo('wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | python2.6')
-    test_setup_python_26()
-
-
 def test_setup_python_27():
     """Test Python 2.7 setup.
     """
@@ -272,8 +209,6 @@ def setup_python_27():
     """
     sudo('apt-get install -y python2.7')
     sudo('apt-get install -y python2.7-dev')
-    # PIL
-    sudo('apt-get install -y python-imaging')
     # LXML
     sudo('apt-get install -y python-lxml')
     # Test Coverage
