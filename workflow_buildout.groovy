aa3b8a456fbd59d6adee8fbf110a62fb5bdc7d25
Timo Stollenwerk <tisto@plone.org>
2015-02-15 00:37:26 +0100

    Add workflow_buildout.groovy.

---
+++ workflow_buildout.groovy
@@ -0,0 +1,20 @@
+/**
+ * Run and archive buildout, then pass it to the next stage.
+ */
+stage 'Build'
+node {
+  sh "rm -rf *"
+  git branch: '5.0', changelog: true, poll: true, url: 'https://github.com/plone/buildout.coredev.git'
+  sh "python2.7 bootstrap.py"
+  sh "bin/buildout -c jenkins.cfg"
+  sh "tar -c -f buildout.tar bin parts develop-eggs var src"
+  archive 'buildout.tar'
+}
+
+stage 'Test'
+node {
+  sh "rm -rf *"
+  unarchive mapping: ['buildout.tar': '.']
+  sh "tar -x -f buildout.tar"
+  sh "ls -al"
+}
