a933e2bbb38aa8ac8dbf9d52a867575c6dd6fa06
Timo Stollenwerk <tisto@plone.org>
2015-02-15 01:14:12 +0100

    Fix passing buildout; add notification.

--- flow.groovy
+++ flow.groovy
@@ -1,30 +1,44 @@
+/**
+ * Run buildout coredev, archive it and pass it to the test stage that runs
+ * test in parallel.
+ */
+
 stage 'Build'
 node {
-  sh "rm -rf ./*"
+  sh "rm -rf *"
   git branch: '5.0', changelog: true, poll: true, url: 'https://github.com/plone/buildout.coredev.git'
   sh "python2.7 bootstrap.py"
   sh "bin/buildout -c jenkins.cfg"
-  step([$class: 'ArtifactArchiver', artifacts: '**/.*', fingerprint: true])
+  sh "tar -c -f buildout.tar bin parts src"
+  archive 'buildout.tar'
 }
 
-stage name: 'Test'
+stage 'Test'
 parallel(
   test1: {
     node {
-      unarchive mapping: ['*': '.']
-      sh "bin/buildout -c jenkins.cfg"
+      sh "rm -rf *"
+      unarchive mapping: ['buildout.tar': '.']
+      sh "tar -x -f buildout.tar"
+      sh "ls -al"
       sh "bin/jenkins-test -s plone.app.discussion"
-      step([$class: 'ArtifactArchiver', artifacts: 'parts/jenkins-test/testreports/*.xml', fingerprint: true])
       step([$class: 'JUnitResultArchiver', testResults: 'parts/jenkins-test/testreports/*.xml'])
     }
   },
   test2: {
     node {
-      unarchive mapping: ['*': '.']
-      sh "bin/buildout -c jenkins.cfg"
+      sh "rm -rf *"
+      unarchive mapping: ['buildout.tar': '.']
+      sh "tar -x -f buildout.tar"
+      sh "ls -al"
       sh "bin/jenkins-test -s plone.app.dexterity"
-      step([$class: 'ArtifactArchiver', artifacts: 'parts/jenkins-test/testreports/*.xml', fingerprint: true])
       step([$class: 'JUnitResultArchiver', testResults: 'parts/jenkins-test/testreports/*.xml'])
     }
   }
 )
+
+stage 'Notification'
+node {
+  stage 'Notification'
+  step([$class: 'Mailer', notifyEveryUnstableBuild: true, recipients: 'tisto@plone.org', sendToIndividuals: true])
+}
