34b048083b2603f368b69fd3f65033ab48c1859f
Gil Forcada <gforcada@gnome.org>
2016-01-24 17:40:34 +0100

    Bump setuptools to 19.4

--- scripts/5.0-at.sh
+++ scripts/5.0-at.sh
@@ -1,4 +1,4 @@
 # buildout and AT tests
-$PYTHON27 bootstrap.py --setuptools-version=18.2 -c jenkins.cfg
+$PYTHON27 bootstrap.py --setuptools-version=19.4 -c jenkins.cfg
 bin/buildout -c jenkins.cfg
 bin/alltests-at --xml
--- scripts/plips.sh
+++ scripts/plips.sh
@@ -1,5 +1,5 @@
 # buildout, core and AT tests
-$PYTHON27 bootstrap.py --setuptools-version=18.2 -c {buildout}
+$PYTHON27 bootstrap.py --setuptools-version=19.4 -c {buildout}
 bin/buildout -c {buildout}
 bin/alltests --xml --all
 bin/alltests-at --xml
