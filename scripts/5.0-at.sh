bf38196bd5c02e8f24b0fbd36248e13c2275ca3d
Gil Forcada <gforcada@gnome.org>
2016-02-12 14:22:59 +0100

    Do a shallow clone on 5.0 jobs

--- scripts/5.0-at.sh
+++ scripts/5.0-at.sh
@@ -1,4 +1,5 @@
 # buildout and AT tests
+sed -i 's/    mr.developer/    mr.developer\ngit-clone-depth = 100/' core.cfg
 $PYTHON27 bootstrap.py --setuptools-version=19.4 -c jenkins.cfg
 bin/buildout -c jenkins.cfg
 bin/alltests-at --xml
--- scripts/5.0-robot.sh
+++ scripts/5.0-robot.sh
@@ -1,4 +1,5 @@
 # buildout and robot tests
+sed -i 's/    mr.developer/    mr.developer\ngit-clone-depth = 100/' core.cfg
 $PYTHON27 bootstrap.py --setuptools-version=19.4 -c jenkins.cfg
 bin/buildout -c jenkins.cfg
 ROBOTSUITE_PREFIX=ONLYROBOT
