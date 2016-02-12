bae773ddaa4cc5cca4f1da990061f80e479797ac
Gil Forcada <gforcada@gnome.org>
2016-02-12 11:20:03 +0100

    Need to cd into the folder

--- scripts/qa-global-code-analysis.sh
+++ scripts/qa-global-code-analysis.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # checkout all packages
 sed -i 's/    mr.developer/    mr.developer\ngit-clone-depth = 100/' jenkins-package-dependencies.cfg
 $PYTHON27 bootstrap.py -c jenkins-package-dependencies.cfg
@@ -13,51 +14,52 @@ cd src
 for pkg in *;
 do
     if [ `echo $blacklist | grep -c $pkg` -eq 0 ]; then
-        cp ../bootstrap.py $pkg/bootstrap.py
-        cp ../experimental/qa.cfg $pkg/qa.cfg
-        cp ../.isort.cfg $pkg/.isort.cfg
 
-        if [[ -d src ]]; then
+        cd $pkg
+        cp ../../bootstrap.py bootstrap.py
+        cp ../../experimental/qa.cfg qa.cfg
+        cp ../../.isort.cfg .isort.cfg
+
+        if [[ -d "src" ]]; then
             sed -i 's#directory = src#directory = src#' qa.cfg
 
-        elif [[ -d plone ]]; then
+        elif [[ -d "plone" ]]; then
             sed -i 's#directory = src#directory = plone#' qa.cfg
 
-        elif [[ -d collective ]]; then
+        elif [[ -d "collective" ]]; then
             sed -i 's#directory = src#directory = collective#' qa.cfg
 
-        elif [[ -d Products ]]; then
+        elif [[ -d "Products" ]]; then
             sed -i 's#directory = src#directory = Products#' qa.cfg
 
-        elif [[ -d five ]]; then
+        elif [[ -d "five" ]]; then
             sed -i 's#directory = src#directory = five#' qa.cfg
 
-        elif [[ -d archetypes ]]; then
+        elif [[ -d "archetypes" ]]; then
             sed -i 's#directory = src#directory = archetypes#' qa.cfg
 
-        elif [[ -d repoze ]]; then
+        elif [[ -d "repoze" ]]; then
             sed -i 's#directory = src#directory = repoze#' qa.cfg
 
-        elif [[ -d plonetheme ]]; then
+        elif [[ -d "plonetheme" ]]; then
             sed -i 's#directory = src#directory = plonetheme#' qa.cfg
 
-        elif [[ -d ZConfig ]]; then
+        elif [[ -d "ZConfig" ]]; then
             sed -i 's#directory = src#directory = ZConfig#' qa.cfg
 
-        elif [[ -d txtfilter ]]; then
+        elif [[ -d "txtfilter" ]]; then
             sed -i 's#directory = src#directory = txtfilter#' qa.cfg
 
-        elif [[ -d transaction ]]; then
+        elif [[ -d "transaction" ]]; then
             sed -i 's#directory = src#directory = transaction#' qa.cfg
 
-        elif [[ -d borg ]]; then
+        elif [[ -d "borg" ]]; then
             sed -i 's#directory = src#directory = borg#' qa.cfg
 
         else
             echo "ooops on $pkg";
         fi
 
-        cd $pkg
         $PYTHON27 bootstrap.py -c qa.cfg
         bin/buildout -c qa.cfg
         ./bin/code-analysis >> ../../qa.txt
