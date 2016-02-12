27a6631c335ad3ce4874708dca2b6aa74a6b7089
Gil Forcada <gforcada@gnome.org>
2016-02-12 15:23:27 +0100

    Do a shallow clone on PLIP jobs

--- scripts/plips.sh
+++ scripts/plips.sh
@@ -1,4 +1,5 @@
 # buildout, core and AT tests
+sed -i 's/    mr.developer/    mr.developer\ngit-clone-depth = 100/' core.cfg
 $PYTHON27 bootstrap.py --setuptools-version=19.4 -c {buildout}
 bin/buildout -c {buildout}
 bin/alltests --xml --all
