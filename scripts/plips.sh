bc1be14a2cbff11436c205946300b3c0ec4256c2
Gil Forcada <gforcada@gnome.org>
2016-02-26 14:43:05 +0100

    Make sure that all tests run on plip jobs

--- scripts/plips.sh
+++ scripts/plips.sh
@@ -3,5 +3,18 @@
 sed -i 's/    mr.developer/    mr.developer\ngit-clone-depth = 100/' core.cfg
 $PYTHON27 bootstrap.py --setuptools-version=19.4 -c {buildout}
 bin/buildout -c {buildout}
-bin/alltests --xml --all
-bin/alltests-at --xml
+
+return_code="all_right"
+
+bin/alltests --xml --all || return_code=$?
+bin/alltests-at --xml || return_code=$?
+
+if [ $return_code = "all_right" ]; then
+    return_code=$?
+fi
+
+# Update GitHub pull request status
+$PYTHON27 templates/pr-update-status.py
+
+# Keep tests return code
+exit $return_code
