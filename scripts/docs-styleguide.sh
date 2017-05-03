3d7aeb9b92434a5c95250a061717f523a85bb292
Gil Forcada <gil.gnome@gmail.com>
2017-05-03 11:03:18 +0200

    Never commit after midnight
    Rather than 31.1.1 the setuptools pin was supposed to be 33.1.1
    
    Fixed.

--- scripts/docs-styleguide.sh
+++ scripts/docs-styleguide.sh
@@ -11,5 +11,5 @@ sed -i 's/^five./#five./' sources.cfg
 sed -i 's/^z3c./#z3c./' sources.cfg
 sed -i 's/^Products.CMF/#Products.CMF/' sources.cfg
 sed -i 's/^Products.MailHost/#Products.MailHost/' sources.cfg
-$PYTHON27 bootstrap.py --setuptools-version 31.1.1 --buildout-version 2.8.0 -c docs.cfg
+$PYTHON27 bootstrap.py --setuptools-version 33.1.1 --buildout-version 2.8.0 -c docs.cfg
 bin/buildout -c docs.cfg install test
