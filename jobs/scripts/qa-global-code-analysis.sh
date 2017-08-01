fc2981146899170ab7fd2b16c42e87c0728c2217
Gil Forcada <gil.gnome@gmail.com>
2017-08-02 00:41:05 +0200

    [virtualenv] Convert missing jobs

--- jobs/scripts/qa-global-code-analysis.sh
+++ jobs/scripts/qa-global-code-analysis.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 # checkout all packages
 sed -i 's/    mr.developer/    mr.developer\ngit-clone-depth = 100/' jenkins-package-dependencies.cfg
-$PYTHON27 bootstrap.py -c jenkins-package-dependencies.cfg
-bin/buildout -c jenkins-package-dependencies.cfg
-
-echo "" > qa.txt
+pip install -r requirements.txt
+buildout -c jenkins-package-dependencies.cfg
 
 sed -i 's#jenkins = True#jenkins = False#' experimental/qa.cfg
 sed -i 's#\[buildout\]#\[buildout\]\nbin-directory = ../bin#' experimental/qa.cfg
 sed -i 's#\[buildout\]#\[buildout\]\nparts-directory = ../parts#' experimental/qa.cfg
 wget https://raw.githubusercontent.com/plone/plone.recipe.codeanalysis/master/.isort.cfg -O .isort.cfg
 
-bin/buildout -c experimental/qa.cfg
+buildout -c experimental/qa.cfg
 
+echo "" > qa.txt
 blacklist="Plone plone.themepreview diazo jquery.recurrenceinput.js mockup mockup-core"
 for pkg in src/*;
 do
