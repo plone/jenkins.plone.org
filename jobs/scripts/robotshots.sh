b13209c12a699a1596e458854e625efe3e0b3dd6
Gil Forcada <gil.gnome@gmail.com>
2017-11-04 17:35:15 +0100

    Use 5.1 and firefox

--- jobs/scripts/robotshots.sh
+++ jobs/scripts/robotshots.sh
@@ -2,4 +2,5 @@
 sed -i 's/    mr.developer/    mr.developer\ngit-clone-depth = 100/' core.cfg
 pip install -r requirements.txt
 buildout -c jenkins.cfg
-bin/pybot -v BROWSER:phantomjs src/Products.CMFPlone/Products/CMFPlone/tests/robot/robodoc
+#bin/pybot -v BROWSER:phantomjs src/Products.CMFPlone/Products/CMFPlone/tests/robot/robodoc
+bin/pybot src/Products.CMFPlone/Products/CMFPlone/tests/robot/robodoc
