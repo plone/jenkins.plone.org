cd1b717016fb053c1f33230ec16e84d69443c579
Gil Forcada <gil.gnome@gmail.com>
2017-11-04 17:31:00 +0100

    WIP: robotshots job

---
+++ jobs/scripts/robotshots.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sed -i 's/    mr.developer/    mr.developer\ngit-clone-depth = 100/' core.cfg
+pip install -r requirements.txt
+buildout -c jenkins.cfg
+bin/pybot -v BROWSER:phantomjs src/Products.CMFPlone/Products/CMFPlone/tests/robot/robodoc
