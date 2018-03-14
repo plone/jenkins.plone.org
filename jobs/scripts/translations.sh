526b29ccf22663eda6a0670d0ce5ef864e52c7f5
Gil Forcada <gil.gnome@gmail.com>
2018-03-14 09:51:25 +0100

    Update translation files before generating the statistics

--- jobs/scripts/translations.sh
+++ jobs/scripts/translations.sh
@@ -2,12 +2,14 @@
 if [ "{plone-version}" = "4.3" ]; then
     sed -i 's/    buildout.dumppickedversions/    buildout.dumppickedversions\ngit-clone-depth = 100/' core.cfg
     python bootstrap.py
-    bin/buildout -c experimental/i18n.cfg install i18ndude
+    bin/buildout -c experimental/i18n.cfg install i18ndude i18n i18n-update-all
 else
     sed -i 's/    mr.developer/    mr.developer\ngit-clone-depth = 100/' core.cfg
     pip install -r requirements.txt
-    buildout -c experimental/i18n.cfg install i18ndude
+    buildout -c experimental/i18n.cfg install i18ndude i18n i18n-update-all
 fi
+
+./bin/i18n-update-all
 
 cd src/plone.app.locales/plone/app/locales/locales
 mkdir reports
