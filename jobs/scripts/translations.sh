31f13c1a4e5aee54251dc67e1289b097d0b4a47d
Gil Forcada <gil.gnome@gmail.com>
2018-03-14 09:17:57 +0100

    Add translations jobs

---
+++ jobs/scripts/translations.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+if [ "{plone-version}" = "4.3" ]; then
+    sed -i 's/    buildout.dumppickedversions/    buildout.dumppickedversions\ngit-clone-depth = 100/' core.cfg
+    python bootstrap.py
+    bin/buildout -c experimental/i18n.cfg install i18ndude
+else
+    sed -i 's/    mr.developer/    mr.developer\ngit-clone-depth = 100/' core.cfg
+    pip install -r requirements.txt
+    buildout -c experimental/i18n.cfg install i18ndude
+fi
+
+cd src/plone.app.locales/plone/app/locales/locales
+mkdir reports
+
+../../../../../../bin/i18ndude list -p plone > reports/plone.txt
+../../../../../../bin/i18ndude list -p atcontenttypes  > reports/atcontenttypes.txt
+../../../../../../bin/i18ndude list -p atreferencebrowserwidget  > reports/atreferencebrowserwidget.txt
+../../../../../../bin/i18ndude list -p cmfeditions  > reports/cmfeditions.txt
+../../../../../../bin/i18ndude list -p cmfplacefulworkflow  > reports/cmfplacefulworkflow.txt
+../../../../../../bin/i18ndude list -p plonefrontpage  > reports/plonefrontpage.txt
+../../../../../../bin/i18ndude list -p plonelocales  > reports/plonelocales.txt
+
+if [ "{plone-version}" = "4.3" ]; then
+    ../../../../../../bin/i18ndude list -p linguaplone  > reports/linguaplone.txt
+    ../../../../../../bin/i18ndude list -p passwordresettool  > reports/passwordresettool.txt
+else
+    ../../../../../../bin/i18ndude list -p widgets  > reports/widgets.txt
+fi
