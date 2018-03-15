568666ce48986377fe7d9a7d731a70ac7c6e6057
Gil Forcada <gil.gnome@gmail.com>
2018-03-15 10:31:33 +0100

    Replace qa-i18n-find-untranslated
    That was a single job that was only checking missing strings on latest
    branch only.
    
    Now we have 4 jobs, one for each major branch.

--- jobs/scripts/i18n-find-untranslated.sh
+++ jobs/scripts/i18n-find-untranslated.sh
@@ -1,6 +1,13 @@
-#!/usr/bin/env bash
-sed -i 's/    mr.developer/    mr.developer\ngit-clone-depth = 100/' core.cfg
-pip install -r requirements.txt
-buildout -c experimental/i18n.cfg install i18n-find-untranslated i18ndude
+#!/bin/bash
+if [ "{plone-version}" = "4.3" ]; then
+    sed -i 's/    buildout.dumppickedversions/    buildout.dumppickedversions\ngit-clone-depth = 100/' core.cfg
+    python bootstrap.py
+    bin/buildout -c experimental/i18n.cfg install i18n-find-untranslated i18ndude
+else
+    sed -i 's/    mr.developer/    mr.developer\ngit-clone-depth = 100/' core.cfg
+    pip install -r requirements.txt
+    buildout -c experimental/i18n.cfg install i18n-find-untranslated i18ndude
+fi
+
 export PYTHONIOENCODING=utf-8
-bin/i18n-find-untranslated details > missing.txt
+bin/i18n-find-untranslated details > missing.txt || echo 0
