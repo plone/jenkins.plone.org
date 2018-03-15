b87ffe681ba2ae11b540724e72e12d8f5b8f627b
Gil Forcada <gil.gnome@gmail.com>
2018-03-15 09:40:34 +0100

    Fix find untranslated strings jobs

--- jobs/scripts/i18n-find-untranslated.sh
+++ jobs/scripts/i18n-find-untranslated.sh
@@ -2,4 +2,5 @@
 sed -i 's/    mr.developer/    mr.developer\ngit-clone-depth = 100/' core.cfg
 pip install -r requirements.txt
 buildout -c experimental/i18n.cfg install i18n-find-untranslated i18ndude
+export PYTHONIOENCODING=utf-8
 bin/i18n-find-untranslated details > missing.txt
