8344f24feac9bfe1ac5f07e1fe6a61f506781b52
Philip Bauer <bauer@starzel.de>
2018-05-09 09:11:29 +0200

    make jenkins actually run some py3-tests

--- jobs/scripts/plip-py3.sh
+++ jobs/scripts/plip-py3.sh
@@ -3,4 +3,4 @@ sed -i 's/    mr.developer/    mr.developer\ngit-clone-depth = 100/' core.cfg
 pip install -r requirements.txt
 buildout -c py3.cfg
 
-timeout 15 ./bin/wsgi.py etc/waitress.ini
+./bin/test -s plone.app.contenttypes
