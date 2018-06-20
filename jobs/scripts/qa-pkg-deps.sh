8c2081286ae6b0181781bab3b6262a3f8d7e11c1
Gil Forcada <gil.gnome@gmail.com>
2018-06-21 00:31:22 +0200

    New jenkins job: package dependencies
    Test a package+branch combination against z3c.dependencychecker and
    report via email and github about it.

---
+++ jobs/scripts/qa-pkg-deps.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# checkout only the package being tested
+cat > checkouts.cfg << EOF
+[buildout]
+git-clone-depth = 10
+always-checkout = force
+auto-checkout =
+    ${PACKAGE_NAME}
+EOF
+
+if grep "${PACKAGE_NAME}" sources.cfg | grep "${BRANCH}" > /dev/null
+then
+    echo "${PACKAGE_NAME} already has ${BRANCH} in sources.cfg"
+else
+    echo "branch of ${PACKAGE_NAME} has been changed to ${BRANCH} in sources.cfg"
+    sed -i "s/\(${PACKAGE_NAME} .*branch=\)[a-z0-9]*/\1${BRANCH}/" sources.cfg
+    grep "${PACKAGE_NAME}" sources.cfg
+fi
+
+pip install -r requirements.txt
+buildout -c core.cfg install dependencies
+
+return_code=0
+./bin/dependencychecker src/${PACKAGE_NAME} > deps.txt || return_code=$?
+
+if [ -f "src/${PACKAGE_NAME}/pyproject.toml" ];
+then
+  python templates/deps-update-status.py
+fi
+
+exit ${return_code}
