0e2fe132d0b55a37941a4266a38a3ee3a030c5eb
Gil Forcada <gil.gnome@gmail.com>
2018-09-05 00:30:54 +0200

    Create firefox and chrome jobs for Plone 5.x jobs

--- jobs/scripts/5.x-robot.sh
+++ jobs/scripts/5.x-robot.sh
@@ -2,5 +2,6 @@
 sed -i 's/    mr.developer/    mr.developer\ngit-clone-depth = 100/' core.cfg
 pip install -r requirements.txt
 buildout -c core.cfg
+ROBOT_BROWSER={browser}
 ROBOTSUITE_PREFIX=ONLYROBOT
 bin/alltests -t ONLYROBOT --all --xml
