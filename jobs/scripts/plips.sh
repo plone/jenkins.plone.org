6739adac10644f7dc4cb3ed922781b428255ebe4
Gil Forcada <gil.gnome@gmail.com>
2018-11-03 16:20:19 +0100

    jobs: fix PLIP's job script

--- jobs/scripts/plips.sh
+++ jobs/scripts/plips.sh
@@ -3,16 +3,7 @@
 pip install -r requirements.txt
 buildout buildout:git-clone-depth=1 -c {buildout}
 
-return_code="all_right"
 export PATH="/usr/lib/chromium-browser:$PATH"
 export ROBOT_BROWSER=chrome
 
-xvfb-run -a --server-args='-screen 0 1920x1200x24' bin/alltests --xml --all || return_code=$?
-xvfb-run -a --server-args='-screen 0 1920x1200x24' bin/alltests-at --xml || return_code=$?
-
-if [ $return_code = "all_right" ]; then
-    return_code=$?
-fi
-
-# Keep tests return code
-exit $return_code
+xvfb-run -a --server-args='-screen 0 1920x1200x24' bin/test --xml --all
