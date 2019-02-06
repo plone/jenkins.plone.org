4ce1db63e48f1389d879ea9243f61ea8ff5f5db5
Gil Forcada <gil.gnome@gmail.com>
2019-02-06 21:14:40 +0100

    feat: allow testing add-ons against python 3.x

--- jobs/scripts/addon-tests.sh
+++ jobs/scripts/addon-tests.sh
@@ -3,8 +3,13 @@ if [ "{plone-version}" = "4.3" ]; then
     python bootstrap.py -c jenkins.cfg
     bin/buildout buildout:git-clone-depth=1 -c jenkins.cfg
 else
-    python bootstrap.py --setuptools-version 33.1.1 --buildout-version 2.8.0 -c core.cfg
-    bin/buildout buildout:git-clone-depth=1 -c core.cfg
+    if [ "{plone-version}" = "5.2" ]; then
+        pip install -r requirements.txt
+        buildout buildout:git-clone-depth=1 -c core.cfg
+    else
+        python bootstrap.py --setuptools-version 33.1.1 --buildout-version 2.8.0 -c core.cfg
+        bin/buildout buildout:git-clone-depth=1 -c core.cfg
+    fi
 fi
 
 bin/test --xml -s ${{ADDON_NAME}}
