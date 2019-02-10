c0fe06a651856d05a00f926bf2cd28fbee91d43a
Gil Forcada <gil.gnome@gmail.com>
2019-02-10 22:46:00 +0100

    Fix addons tests

--- jobs/scripts/addon-tests.sh
+++ jobs/scripts/addon-tests.sh
@@ -4,13 +4,8 @@ if [ "{plone-version}" = "4.3" ]; then
     python bootstrap.py -c jenkins.cfg
     bin/buildout $OPTIONS -c jenkins.cfg
 else
-    if [ "{plone-version}" = "5.2" ]; then
-        pip install -r requirements.txt
-        buildout $OPTIONS -c core.cfg
-    else
-        python bootstrap.py --setuptools-version 33.1.1 --buildout-version 2.8.0 -c core.cfg
-        bin/buildout $OPTIONS -c core.cfg
-    fi
+    pip install -r requirements.txt
+    buildout $OPTIONS -c core.cfg
 fi
 
 bin/test --xml -s ${{ADDON_NAME}}
