f10d79f8601e1ebfa293160fc27f95e5b5be6296
Gil Forcada Codinachs <gil.gnome@gmail.com>
2021-09-30 10:05:48 +0200

    cleanup: more leftovers

--- jobs/scripts/4.3-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# run buildout and tests
-{pybinary} bootstrap.py -c jenkins.cfg
-bin/buildout buildout:git-clone-depth=1 -c jenkins.cfg
-
-export PATH="/usr/lib/chromium-browser:$PATH"
-export ROBOT_BROWSER=chrome
-
-xvfb-run -a --server-args='-screen 0 1920x1200x24' bin/jenkins-alltests -1
