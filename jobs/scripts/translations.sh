e11f2be69eb6da18f0758bb49a2398d1379a7684
Gil Forcada Codinachs <gil.gnome@gmail.com>
2022-11-19 01:35:14 +0100

    cleanup(jobs/scripts): apply rotonen suggestions

--- jobs/scripts/translations.sh
+++ jobs/scripts/translations.sh
@@ -4,20 +4,22 @@ buildout buildout:git-clone-depth=1 -c experimental/i18n.cfg install i18ndude i1
 
 ./bin/i18n-update-all
 
-cd src/plone.app.locales/plone/app/locales/locales
+cd src/plone.app.locales/plone/app/locales/locales || exit 1
 mkdir reports
 
 export PYTHONIOENCODING=utf-8
 
-../../../../../../bin/i18ndude list -p plone > reports/plone.txt
-../../../../../../bin/i18ndude list -p cmfeditions  > reports/cmfeditions.txt
-../../../../../../bin/i18ndude list -p cmfplacefulworkflow  > reports/cmfplacefulworkflow.txt
-../../../../../../bin/i18ndude list -p plonefrontpage  > reports/plonefrontpage.txt
-../../../../../../bin/i18ndude list -p plonelocales  > reports/plonelocales.txt
+alias i18ndude '../../../../../../bin/i18ndude'
 
-../../../../../../bin/i18ndude list -p widgets  > reports/widgets.txt
+i18ndude list -p plone > reports/plone.txt
+i18ndude list -p cmfeditions  > reports/cmfeditions.txt
+i18ndude list -p cmfplacefulworkflow  > reports/cmfplacefulworkflow.txt
+i18ndude list -p plonefrontpage  > reports/plonefrontpage.txt
+i18ndude list -p plonelocales  > reports/plonelocales.txt
+
+i18ndude list -p widgets  > reports/widgets.txt
 
 if [ "{plone-version}" != "6.0" ]; then
-../../../../../../bin/i18ndude list -p atcontenttypes  > reports/atcontenttypes.txt
-../../../../../../bin/i18ndude list -p atreferencebrowserwidget  > reports/atreferencebrowserwidget.txt
+  i18ndude list -p atcontenttypes  > reports/atcontenttypes.txt
+  i18ndude list -p atreferencebrowserwidget  > reports/atreferencebrowserwidget.txt
 fi
--- jobs/scripts/undeclared-dependencies.sh
+++ jobs/scripts/undeclared-dependencies.sh
@@ -17,12 +17,11 @@ pip install -r requirements.txt
 buildout -c core.cfg install dependencies
 
 # get all dependencies
-echo "" > deps.txt
 for pkg in src/*;
 do
-    echo "$pkg" >> deps.txt
-    ./bin/dependencychecker --exit-zero $pkg >> deps.txt || echo "Failed for $pkg"
-done
+    echo "$pkg"
+    ./bin/dependencychecker --exit-zero $pkg
+done | tee deps.txt
 
 LINES=`wc -l deps.txt | cut -d" " -f1`
 echo "YVALUE=${LINES}" > lines.log
