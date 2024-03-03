5359bd2be7c32209627de4b20c7523de4cf45aa4
Gil Forcada Codinachs <gil.gnome@gmail.com>
2024-03-03 19:27:41 +0100

    fix(jobs): improve PLIPs script
    And remove usage of `shining-panda` plugin.

--- jobs/scripts/plips.sh
+++ jobs/scripts/plips.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 set -x
 
+PYTHON_VERSION="{py}"
+PLONE_VERSION="{plone-version}"
+
+/srv/python${{PYTHON_VERSION}}/bin/python3 -m venv venv
+. venv/bin/activate
+
 pip install -r requirements.txt
 
 buildout buildout:git-clone-depth=1 -c {buildout}
 
 export PATH="/usr/lib/chromium-browser:$PATH"
-export ROBOT_BROWSER=headlesschrome
+export ROBOT_BROWSER="headlesschrome"
+export ROBOTSUITE_PREFIX=ONLYROBOT
 export PYTHONWARNINGS='ignore'
+
+export PLAYWRIGHT_BROWSERS_PATH='/home/jenkins/robot-browsers/'
+bin/rfbrowser init
 
 bin/test --xml --all
